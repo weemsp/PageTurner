685449b6491f28d88447e8a433891141bcafad54
weemsp <weemsp@udel.edu>
2019-12-03 15:00:48 -0500

    i want to not do this

--- main.js
+++ main.js
@@ -2,10 +2,16 @@ const FPS = 30;
 var pagenumDisplay;
 var modeSelector;
 var setBlinking = false;
+var book;
+var textHere;
+var _content;
 
 function begin() {
 	console.log("please work");
 	//webgazer.begin();
+	book = document.getElementById("Book");
+	textHere = document.getElementById("TextHere");
+	textHere.innerHTML = LOREM_IPSUM;
 	engine.run();
 }
 
@@ -29,6 +35,12 @@ var engine = {
 			setBlinking = true;
 			stupidScopeEscaper.onExampleChosen({index: 20, label: "advanced - face tracking - blink detection", value: "advanced - face tracking - blink detection"});
 		}
+		if (_content) {
+			_content.style.position = "absolute";
+			_content.style.left = "0";
+			_content.style.top = "0";
+		} else
+			_content = document.getElementById("content");
 		/*var dick = webgazer.getPrediction();
 		if (dick) {
 			console.log(dick.x + ", " + dick.y);
@@ -42,11 +54,15 @@ var engine = {
 }
 
 function pageLeft() {
-	pagenum--;
-	pagenumDisplay.innerHTML = pagenum;
+	if (pagenum > 1) {
+		pagenum--;
+		pagenumDisplay.innerHTML = pagenum;
+		book.scrollTop -= 800;
+	}
 }
 
 function pageRight() {
 	pagenum++;
 	pagenumDisplay.innerHTML = pagenum;
+	book.scrollTop += 800;
 }
